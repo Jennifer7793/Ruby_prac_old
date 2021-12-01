aa88ac62527f2cb6a6dae3c1cd132edfa6779ab4
LAPTOP-56NU4RUS\jenni <jennifer7793@gmail.com>
2021-12-01 22:07:11 +0800

    1201

---
+++ 1201Day7/aaa.js
@@ -0,0 +1,11 @@
+const hello = () => {
+  console.log('hello');
+};
+
+function hi() {}
+function a() {}
+function b() {}
+export function c() {}
+
+export { a, b }
+export default hello;
---
+++ 1201Day7/bbb.js
@@ -0,0 +1,3 @@
+import hello from "./aaa.js" //預設export
+
+import { a, b } from "./aaa.js" //非預設，可選擇性取
